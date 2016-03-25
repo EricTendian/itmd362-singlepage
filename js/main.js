5bc7996b17b84e7776b323dbd1349f48003388f2
Eric Tendian <erictendian@gmail.com>
2016-03-24 23:24:48 -0500

    Lots of design changes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-$('html').removeClass('no-js');
+$('html').removeClass('no-js').addClass('js');
+
+$('ul.tab-nav li a').on('click', function () {
+  $(this).parent().parent().find('li').removeClass('active');
+  $(this).parent().addClass('active');
+  $(this).parent().parent().siblings('.tab-container').children().removeClass('active');
+  $($(this).attr('href')).addClass('active');
+  return false;
+});
