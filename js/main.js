33a49b06a2ca74f2455eccf328e9a7668e5b59a3
Eric Tendian <erictendian@gmail.com>
2016-03-25 18:11:04 -0500

    Adding background images, removing commented out code, more styling updates

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 $('html').removeClass('no-js').addClass('js');
 
 $('ul.tab-nav li a').on('click', function () {
-  $(this).parent().parent().find('li').removeClass('active');
+  var grandparent = $(this).parent().parent();
+  grandparent.find('li').removeClass('active');
   $(this).parent().addClass('active');
-  $(this).parent().parent().siblings('.tab-container').children().removeClass('active');
+  grandparent.siblings('.tab-container').children().removeClass('active');
   $($(this).attr('href')).addClass('active');
   return false;
 });
